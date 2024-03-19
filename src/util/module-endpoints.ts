4028efe6ea0506f34bac64bfc57b9a04b4ec7380
Aladeenb <alabtk@gmail.com>
2024-03-19 16:15:14 +0100

    Changed module address

--- src/util/module-endpoints.ts
+++ src/util/module-endpoints.ts
@@ -1,2 +1,2 @@
-export const RENA_MODULE_TESTNET = "0xda49117bbf154c8b59223a03885abe480e615b24ee7e92daf3ac7bd6e4e04535::core";
+export const RENA_MODULE_TESTNET = "0x91c62bc900d7ab1d4c699b293d82c754a68369af278f7422ef5eadcbfed8efbd::core";
 export const RENA_MODULE_MAINNET = "<address>::core";

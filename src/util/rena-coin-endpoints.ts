f900f945e55215baa14406e4e01bd980e97dc668
Aladeenb <alabtk@gmail.com>
2024-03-20 13:35:51 +0100

    Added coins related endpoints and updated functions

---
+++ src/util/rena-coin-endpoints.ts
@@ -0,0 +1,2 @@
+export const RENA_COIN_TYPE_TESTNET = "0x91c62bc900d7ab1d4c699b293d82c754a68369af278f7422ef5eadcbfed8efbd::core::RenegadeCoin"
+export const LIQUID_COIN_OBJECT_TESTNET = "0xfdf8c24445a8448cea35388b4018fff140be99d4ef57888d2da8f662afa97d0f"

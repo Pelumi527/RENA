615b3d99c103eb625dad17467182ea73fe75c902
Aladeenb <alabtk@gmail.com>
2024-03-20 21:24:13 +0100

    Fixed Cannot read properties of undefined and redeployed modules

--- src/util/module-endpoints.ts
+++ src/util/module-endpoints.ts
@@ -1,7 +1,7 @@
 // Purpose: Contains the module endpoints, function names and coin endpoints for the Renegade Network
 
 // module endpoints
-export const RENA_MODULE_TESTNET = "0x91c62bc900d7ab1d4c699b293d82c754a68369af278f7422ef5eadcbfed8efbd::core";
+export const RENA_MODULE_TESTNET = "0xa408eaf6de821be63ec47b5da16cbb5a3ab1af6a351d0bab7b6beddaf7802776::core";
 export const RENA_MODULE_MAINNET = "<address>::core";
 
 // function names
@@ -9,5 +9,5 @@ export const CLAIM = "claim";
 export const LIQUIFY = "liquify";
 
 // coin endpoints
-export const RENA_COIN_TYPE_TESTNET = "0x91c62bc900d7ab1d4c699b293d82c754a68369af278f7422ef5eadcbfed8efbd::core::RenegadeCoin"
-export const LIQUID_COIN_OBJECT_TESTNET = "0xfdf8c24445a8448cea35388b4018fff140be99d4ef57888d2da8f662afa97d0f"
+export const RENA_COIN_TYPE_TESTNET = "0xa408eaf6de821be63ec47b5da16cbb5a3ab1af6a351d0bab7b6beddaf7802776::core::RenegadeCoin"
+export const LIQUID_COIN_OBJECT_TESTNET = "0x3ff43e3d26d8db230c2f2cbc8770bac9e5b9f0dae123d793e98bf526dd09f1e8"

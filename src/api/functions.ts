6c7f847f5d70dbb1c1a5b326cb5ca05d7cd8b002
Aladeenb <alabtk@gmail.com>
2024-03-20 15:15:30 +0100

    Update submit on-chain functions

--- src/api/functions.ts
+++ src/api/functions.ts
@@ -1,53 +1,19 @@
 import {
-    Account,
     AnyNumber,
-    Network,
-    NetworkToNetworkName,
-    AptosConfig,
-    Aptos,
-    MoveAddressType
+    MoveAddressType,
 } from "@aptos-labs/ts-sdk";
 import { RENA_MODULE_TESTNET } from "../util/module-endpoints";
 import { LIQUID_COIN_OBJECT_TESTNET, RENA_COIN_TYPE_TESTNET } from "../util/rena-coin-endpoints";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
 
-// Setup the client
-const APTOS_NETWORK: Network = process.env.APTOS_NETWORK
-  ? NetworkToNetworkName[process.env.APTOS_NETWORK] || Network.TESTNET
-  : Network.TESTNET;
-const config = new AptosConfig({ network: APTOS_NETWORK });
-const aptos = new Aptos(config);
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const { account, signAndSubmitTransaction } = useWallet();
 
 /**
  * Class for interracting with entry functions from core.move
  * only claim and liquify are implemented
  */
 export class Functions {
-  private async submitTransaction(
-    account: Account,
-    funcName: string,
-    typeArgs: string[],
-    args: any[],
-  ) {
-    const transaction = await aptos.transaction.build.simple({
-      sender: account.accountAddress,
-      data: {
-        function: `${RENA_MODULE_TESTNET}::${funcName}`,
-        typeArguments: typeArgs,
-        functionArguments: args,
-      },
-    });
-
-    const senderAuthenticator = aptos.transaction.sign({
-      signer: account,
-      transaction,
-    });
-    const pendingTxn = await aptos.transaction.submit.simple({
-      transaction,
-      senderAuthenticator,
-    });
-
-    return pendingTxn.hash;
-  }
 
   /**
    * Claim X amount of coins given X amount of tokens
@@ -58,15 +24,22 @@ export class Functions {
    */
   // :!:claim
   async claim(
-    sender: Account,
     count: AnyNumber,
   ) {
-    return this.submitTransaction(
-      sender,
-      "claim",
-      [RENA_COIN_TYPE_TESTNET],
-      [LIQUID_COIN_OBJECT_TESTNET, count],
-    );
+    if (account) {
+      try {
+        const res = await signAndSubmitTransaction({
+          sender: account.address,
+          data: {
+            function: `${RENA_MODULE_TESTNET}::${"claim"}`,
+            functionArguments: [LIQUID_COIN_OBJECT_TESTNET, count],
+          }
+        })
+        console.log(res);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 
   /**
@@ -78,15 +51,23 @@ export class Functions {
    */
   // :!:liquify
   async liquify(
-    sender: Account,
     tokens: MoveAddressType[]
   ) {
-    return this.submitTransaction(
-      sender,
-      "liquify",
-      [RENA_COIN_TYPE_TESTNET],
-      [LIQUID_COIN_OBJECT_TESTNET, tokens],
-    );
+    if (account) {
+      try {
+        const res = await signAndSubmitTransaction({
+          sender: account.address,
+          data: {
+            function: `${RENA_MODULE_TESTNET}::${"liquify"}`,
+            typeArguments: [RENA_COIN_TYPE_TESTNET],
+            functionArguments: [LIQUID_COIN_OBJECT_TESTNET, tokens],
+          }
+        })
+        console.log(res);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 
 }
